82d428255823da3f10524e9228bf8ef6313ec7f3
andres-geotec <andres.geotec@gmail.com>
2023-05-03 14:34:35 -0600

    iniciando componentes

---
+++ src/componentes/SisdaiGraficas.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    require: true,
+  },
+  tituloEje: {
+    type: String,
+    default: 'este es el titulo',
+  },
+  tooltip: {
+    type: Function,
+  },
+})
+</script>
+
+<template>
+  <div>hola soy una grafica</div>
+</template>
