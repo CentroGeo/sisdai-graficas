6a95dea2121cacded927ce884524a5d4a90b3186
jdanielgoh <48138542+jdanielgoh@users.noreply.github.com>
2023-06-01 16:25:51 -0600

    Implementando el tema de pintar las barras con funciones enter, join y exit de d3

--- docs/.vuepress/components/prueba/basico.vue
+++ docs/.vuepress/components/prueba/basico.vue
@@ -23,13 +23,14 @@ const margen = ref(30)
     >
       <SisdaiBarras
         :datos="[
-          { categoria: 'aguascalientes', cantidad: 100 },
-          { categoria: 'baja_cal', cantidad: 80 },
-          { categoria: 'baja_cal_sur', cantidad: 20 },
-          { categoria: 'zacatecas', cantidad: 20 },
+          { categoria: 'aguascalientes', cantidad_1: 100, cantidad_2: 100 },
+          { categoria: 'baja_cal', cantidad_1: 80, cantidad_2: 100 },
+          { categoria: 'baja_cal_sur', cantidad_1: 20, cantidad_2: 100 },
+          { categoria: 'zacatecas', cantidad_1: 20, cantidad_2: 100 },
         ]"
         :variables="[
-          { id: 'cantidad', nombre_subcategoria: '$ pesos', color: 'red' },
+          { id: 'cantidad_1', nombre_subcategoria: '$ pesos 1', color: 'red' },
+          { id: 'cantidad_2', nombre_subcategoria: '$ pesos 2', color: 'blue' },
         ]"
       />
     </SisdaiGraficas>
--- src/componentes/sisdai-barras/SisdaiBarras.vue
+++ src/componentes/sisdai-barras/SisdaiBarras.vue
@@ -3,6 +3,8 @@ import { max, sum } from 'd3-array'
 import { axisBottom, axisLeft } from 'd3-axis'
 import { scaleBand, scaleLinear } from 'd3-scale'
 import { select } from 'd3-selection'
+import { stack } from 'd3-shape'
+import { transition } from 'd3-transition'
 
 import { onMounted, onUnmounted, ref, shallowRef, toRefs, watch } from 'vue'
 import usarRegistroGraficas from './../../composables/usarRegistroGraficas'
@@ -44,24 +46,30 @@ const props = defineProps({
 
 const sisdaiBarras = shallowRef()
 const { datos, clave_categorias } = toRefs(props)
-
+transition
 const margenesSvg = ref({})
 
 const escalaBanda = ref(),
   escalaLineal = ref()
+const grupoContenedor = ref(),
+  data_apilada = ref(),
+  grupoBarras = ref(),
+  rectangulos = ref()
+
 function calcularEscalas(grupoVis) {
   if (!grupoVis && grupoVis.ancho === 0) return
 
   escalaBanda.value = scaleBand()
     .domain(datos.value?.map(d => d[clave_categorias.value]))
     .range([0, grupoVis.ancho])
+    .padding(0.5)
 
   escalaLineal.value = scaleLinear()
     .domain([
       0,
       max(datos.value?.map(d => sum(props.variables.map(dd => d[dd.id])))),
     ])
-    .range([0, grupoVis.alto])
+    .range([grupoVis.alto, 0])
 
   select(`div#${idGrafica} svg g.eje-x-abajo`).call(
     axisBottom(escalaBanda.value)
@@ -70,11 +78,50 @@ function calcularEscalas(grupoVis) {
     axisLeft(escalaLineal.value)
   )
 }
+function creaBarras() {
+  data_apilada.value = stack().keys(props.variables.map(d => d.id))(datos.value)
+  grupoBarras.value = grupoContenedor.value
+    .selectAll('g.subcategoria-barras')
+    .data(data_apilada.value)
+    .enter()
+    .append('g')
+    .join(
+      enter => enter.append('g'),
+      null, // no update function
+      exit => {
+        exit.remove()
+      }
+    )
+    .attr('fill', d => props.variables.filter(v => v.id === d.key)[0].color)
+    .attr('class', 'subcategoria-barras')
 
+  rectangulos.value = grupoBarras.value
+    .selectAll('rect.barras')
+    .data(d => d)
+    .join(
+      enter => enter.append('rect'),
+
+      join => join,
+      exit => {
+        exit.remove()
+      }
+    )
+    .attr('class', 'barras')
+  console.log(rectangulos.value)
+}
+function configurarBarras() {
+  rectangulos.value
+    .transition()
+    .duration(500)
+    .attr('x', d => escalaBanda.value(d.data[clave_categorias.value]))
+    .attr('y', d => escalaLineal.value(d[1]))
+    .attr('width', escalaBanda.value.bandwidth())
+    .attr('height', d => escalaLineal.value(d[0]) - escalaLineal.value(d[1]))
+}
 onMounted(() => {
   console.log('SisdaiBarras')
   idGrafica = buscarIdContenedorHtmlSisdai('grafica', sisdaiBarras.value)
-
+  grupoContenedor.value = select('#' + idGrafica + ' svg g.contenedor-barras')
   // console.log(usarRegistroGraficas().grafica(idGrafica))
 
   margenesSvg.value = usarRegistroGraficas().grafica(idGrafica).margenes
@@ -82,11 +129,17 @@ onMounted(() => {
     () => usarRegistroGraficas().grafica(idGrafica).margenes,
     nv => (margenesSvg.value = nv)
   )
-
+  creaBarras()
   calcularEscalas(usarRegistroGraficas().grafica(idGrafica).grupoVis)
+
   watch(
     () => usarRegistroGraficas().grafica(idGrafica).grupoVis,
-    calcularEscalas
+    () => {
+      calcularEscalas(usarRegistroGraficas().grafica(idGrafica).grupoVis)
+      if (usarRegistroGraficas().grafica(idGrafica).grupoVis.ancho > 0) {
+        configurarBarras()
+      }
+    }
   )
 })
 
@@ -99,6 +152,7 @@ onUnmounted(() => {})
     :transform="`translate(${margenesSvg?.izquierda || 0},${
       margenesSvg?.arriba || 0
     })`"
+    class="contenedor-barras"
   >
     <circle
       r="10"
